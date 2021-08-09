b8fb7a915a6896a2cb16d76794439811590cca9c
Koray Erdem KILIÇ <developerkoray@gmail.com>
2021-08-09 14:18:55 +0300

    downloaded files

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const initialState = {
+counter:0,
+
+};
+
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello Redux</Text>
+    </SafeAreaView>
+  );
+};
+
+const First = () => {
+return (
+
+);
+};
